30e9ff3fd741c97452fe20038eddefb1110247bf
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-01-07 14:42:20 +0200

    UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,17 +1,16 @@
-/** String processing exercise 2. */
 public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(uniqueChars(str));
     }
 
-    /**
-     * Returns a string which is identical to the original string, 
-     * except that all the duplicate characters are removed,
-     * unless they are space characters.
-     */
-    public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
-    }
-}
+        public static String uniqueChars(String s) {
+            String x = "";
+            for (int i = 0; i < s.length(); i++) {
+                if (x.indexOf(s.charAt(i)) == -1){
+                    x = x + s.charAt(i);
+                }
+            }
+            return x;
+        }
+ }
